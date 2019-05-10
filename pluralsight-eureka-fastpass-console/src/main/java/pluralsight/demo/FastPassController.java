6cc035fc8ab21ecdc49005a6f139183554b41166
chrisdoberman <chris.herb@gmail.com>
2019-05-09 22:48:33 -0400

    demonstrate ribbon load balancing without eureka

--- pluralsight-eureka-fastpass-console/src/main/java/pluralsight/demo/FastPassController.java
+++ pluralsight-eureka-fastpass-console/src/main/java/pluralsight/demo/FastPassController.java
@@ -1,10 +1,10 @@
 package pluralsight.demo;
 
-import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.web.client.RestTemplateBuilder;
 import org.springframework.cloud.client.loadbalancer.LoadBalanced;
+import org.springframework.cloud.netflix.ribbon.RibbonClient;
 import org.springframework.context.annotation.Bean;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -13,6 +13,7 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.client.RestTemplate;
 
 @Slf4j
+@RibbonClient(name = "pluralsight-fastpass-service-local")
 @Controller
 public class FastPassController {
 
@@ -25,15 +26,19 @@ public class FastPassController {
 	@Autowired
 	private RestTemplate restTemplate;
 
-	@HystrixCommand(fallbackMethod = "getFastPassCustomerDetailsBackup")
+    //disable hystrix command since we're running ribbon without eureka
+    //@HystrixCommand(fallbackMethod = "getFastPassCustomerDetailsBackup")
 	@RequestMapping(path="/customerdetails", params={"fastpassid"})
 	public String getFastPassCustomerDetails(@RequestParam String fastpassid, Model m) {
 		
 //		RestTemplate rest = new RestTemplate();
 //		FastPassCustomer c = rest.getForObject("http://localhost:8086/fastpass?fastpassid=" + fastpassid, FastPassCustomer.class);
 		FastPassCustomer c =
-				restTemplate.getForObject("http://pluralsight-fastpass-service/fastpass?fastpassid=" + fastpassid,
-				FastPassCustomer.class);
+                restTemplate.getForObject("http://pluralsight-fastpass-service-local/fastpass?fastpassid=" + fastpassid,
+                        FastPassCustomer.class);
+        // not using eureka service name
+        //restTemplate.getForObject("http://pluralsight-fastpass-service/fastpass?fastpassid=" + fastpassid,
+        //FastPassCustomer.class);
 		log.info("retrieved customer details");
 		m.addAttribute("customer", c);
 		return "console";
--- pluralsight-eureka-fastpass-console/src/main/java/pluralsight/demo/PluralsightEurekaFastpassConsoleApplication.java
+++ pluralsight-eureka-fastpass-console/src/main/java/pluralsight/demo/PluralsightEurekaFastpassConsoleApplication.java
@@ -2,11 +2,9 @@ package pluralsight.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
-import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
-@EnableCircuitBreaker
-@EnableEurekaClient
+//@EnableCircuitBreaker
+//@EnableEurekaClient
 @SpringBootApplication
 public class PluralsightEurekaFastpassConsoleApplication {
 
