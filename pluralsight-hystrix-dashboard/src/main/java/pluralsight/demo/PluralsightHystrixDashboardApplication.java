5a2f3f802678ba3730693c820119f409269f9c8c
chrisdoberman <chris.herb@gmail.com>
2019-05-09 22:14:38 -0400

    turbine cluster working, but having problems with turbine stream, see issue #1

--- pluralsight-hystrix-dashboard/src/main/java/pluralsight/demo/PluralsightHystrixDashboardApplication.java
+++ pluralsight-hystrix-dashboard/src/main/java/pluralsight/demo/PluralsightHystrixDashboardApplication.java
@@ -5,7 +5,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboard;
 import org.springframework.cloud.netflix.turbine.EnableTurbine;
 
-@EnableTurbine
+@EnableTurbine //disable when aggregrating via message broker since we want to consume from rabbit instead of pulling
+// from each client service for metrics
 @EnableHystrixDashboard
 @SpringBootApplication
 public class PluralsightHystrixDashboardApplication {
